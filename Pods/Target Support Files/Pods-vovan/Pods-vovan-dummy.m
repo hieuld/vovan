62920a4468b6773a7ae0f199967389967ac9449d
chipbk10 <hieu.luong@devoteam.com>
2016-08-31 09:08:29 +0200

    .gitignore is now working

--- Pods/Target Support Files/Pods-vovan/Pods-vovan-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_vovan : NSObject
-@end
-@implementation PodsDummy_Pods_vovan
-@end
--- Pods/Target Support Files/Pods-vovanUITests/Pods-vovanUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_vovanUITests : NSObject
-@end
-@implementation PodsDummy_Pods_vovanUITests
-@end
